5cf808c0ebc307a4b9b9a824e0bb8e5b9348ee13
Osho.Jacob <osho.jacob@unity-bars.com>
2023-01-30 23:28:25 +0100

    added EFCore

---
+++ NotesBackEnd/NotesPersistence/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace NotesPersistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
