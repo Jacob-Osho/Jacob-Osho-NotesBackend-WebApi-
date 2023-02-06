af1ee93f60d33c5962436b487afd8b1a1f88abd6
Osho.Jacob <osho.jacob@unity-bars.com>
2023-02-06 19:36:56 +0100

    dependecy injection

---
+++ NotesBackEnd/NotesApplication/DependecyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+
+
+namespace NotesApplication
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            {
+                services.AddMediatR(Assembly.GetExecutingAssembly());
+                return services;
+            }
+        }
+    }
+}
